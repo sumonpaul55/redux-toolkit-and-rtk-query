ed2949ee4734aa495f1bd6d526f288e858c8f314
sumonpaul55 <sumpaul100@gmail.com>
2024-06-30 17:43:39 +0600

    redux projet intialized and settedup with shade cn

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <div>
+      <h2 className='font-bold text-lg font-serif text-center'>Redux Todo wiht React</h2>
+      <Button>My button</Button>
+    </div>
+  )
+}
+
+
+export default App
